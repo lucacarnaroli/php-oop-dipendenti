09baf2a904dd73d33b6dfaaff01b616a180be13a
luca <luca.carnaroli98@gmail.com>
2020-03-12 12:57:36 +0100

    es

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-require_once __DIR__ . '/azienda/dipendenti.php';
-require_once __DIR__ . '/azienda/dirigente.php';
-require_once __DIR__ . '/azienda/operaio.php';
+require_once __DIR__ . '/classes/dipendenti.php';
+require_once __DIR__ . '/classes/dirigente.php';
+require_once __DIR__ . '/classes/operaio.php';
 
-$luca = new Dirigente('12345', 'Luca', 'Carnaroli', 'via Roma', 'cbvnusahgan', 'it77sdvsadv31e31');
+$luca = new Dirigente('12345', 'luca', 'carnaroli   ', 'via Roma', 'cbvnusahgan', 'it77sdvsadv31e31', 3);
 var_dump($luca);
 
 try {
@@ -15,7 +15,6 @@ var_dump($luca);
 
 echo $luca->getBonus();
 
-echo $luca->viewDipendente();
+// echo $luca->visualizzaDipendente();
+var_dump($luca->visualizzaDipendente());
 
-
-var_dump($luca->viewDipendente());
